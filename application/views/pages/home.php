a0b09e4b807ffc49592e8252cc6eb8c3e456cbfa
unknown <sachinv@langoor.net>
2013-11-11 18:52:17 +0530

    asd

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -22,7 +22,8 @@
           <h2>Warm welcome</h2>
         </div>
       </div>
-    </div></div>
+  </div>
+</div>
 <div class="wrapper-2" data-type="background">
   <div class="text-info">
     <p>CoVended is actively playing a new and emerging role in the DIgital Business World. We are neither a technology vendor nor a consultant, but a "trusted" supporting partner who deeply listens, understands and helps sort out the many conflicting and confusing IT challenges. With this approach we can help clarify and energize powerful new business opportunies for you and your business.</p>
@@ -85,4 +86,72 @@
         </div>  
      </div>
 </div>
-<div class="wrapper-4" data-type="background"></div>
+<div class="wrapper-4" data-type="background">
+  <div class="title">
+   <h2>SERVICES</h2>
+  </div>
+  <div class="services-image">
+    <div class="outer-circle">
+      <div class="logo">
+        <img src="<?php echo base_url('images/covended/circle-logo.png')?>" alt=""/>
+      </div>
+      <div class="app-dev"><span>APPLICATION DEVELOPMENT</span><img src="<?php echo base_url('images/covended/application.png')?>" alt =""/></div>
+      <div class="mob-tech"><span>MOBILE TECHNOLOGY</span><img src="<?php echo base_url('images/covended/mobile.png')?>" alt =""/></div>
+      <div class="graph-des"><span>GRAPHIC DESIGNING AND BRANDING</span><img src="<?php echo base_url('images/covended/graphics.png')?>" alt =""/></div>
+      <div class="soft-test"><span>SOFTWARE TESTING</span><img src="<?php echo base_url('images/covended/testing.png')?>" alt =""/></div>
+      <div class="prod-engg"><span>PRODUCT ENGINEERING</span><img src="<?php echo base_url('images/covended/product.png')?>" alt =""/></div>
+      <div class="web-tech"><span>WEB TECHNOLOGY</span><img src="<?php echo base_url('images/covended/web.png')?>" alt =""/></div>
+    </div>
+
+  </div>
+</div>
+<div class="wrapper-5"></div>
+<div class="wrapper-6">
+  <div class="contact-us">
+   <h2>CONTACT US</h2>
+  </div>
+  <div class="contact-info">
+    <div class="info-banner">
+      <div class="info-banner-img">
+        <p class="just">JUST</p>
+        <p class="say-hi">SAY HI</p>
+      </div>
+    </div>
+    <div class="info-details">
+      <form>
+          <fieldset>
+              <div><label for="uname" class="error">Name</label> <input id="uname" type="text" name="uname" value="" class="error"></div>
+              <div><label for="email">Email</label> <input id="email" type="text" name="email" value=""></div>
+              <div><label for="fname">Phone</label> <input id="fname" type="text" name="fname" value=""></div>
+              <div><label for="lname">So How You?</label> <textarea id="lname" type="text" name="lname" value="" rows="5" cols="30"></textarea></div>
+              <button class="send-submit-form">SEND</button>
+          </fieldset>
+
+     </form>
+    </div>
+  </div>
+  <div class="extra-info">
+    <div class="phone-info">
+      <span class="info-img-1"><img src="<?php echo base_url('images/covended/hello.png')?>" alt=""></span>
+      <p class="p1">SAY HELLO</p>
+      <p>Just give a call to say "Hi !!!"</p>
+      <p class="phone">+91-(0)-990 250 4475</p>
+      <p class="phone">+91-(0)-973 909 5181</p>
+    </div>
+    <div class="address-info">
+      <span class="info-img-2"><img src="<?php echo base_url('images/covended/office_address.png')?>" alt=""></span>
+      <p class="p1">OFFICE ADDRESS</p>
+      <p>No. 402, 3rd floor, Silver Nest Apartment <br/>Harlur Road, Kudlu, Bangalore-560102<br/>Karnataka, India.</p>
+    </div>
+    <div class="mailing-address">
+      <span class="info-img-3"><img src="<?php echo base_url('images/covended/mail.png')?>" alt=""></span>
+      <p class="p1">MAILING ADDRESS</p>
+      <p>Drop us a line via email</p>
+      <p class="email-p">covended@gmail.com</p>
+    </div>
+</div>
+</div>
+ <div class="footer">
+        <span class="copyright">Copyright 2013 covended.com</span>
+        <span class="design">Designed by LongNeck</span>
+</div>
